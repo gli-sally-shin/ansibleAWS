0ceabd19138f17732449d21e6b1881ba7b35d1e4
gli-sally-shin <sally.shin@gausslabs.ai>
2024-06-25 12:48:24 +0900

    jenkins files

--- login.sh
+++ login.sh
@@ -8,3 +8,9 @@ ssh -i "SallyAWS.pem" ec2-user@ec2-34-235-168-107.compute-1.amazonaws.com
 
 ssh -i "~/.ssh/SallyAWS.pem" ec2-user@ec2-34-235-168-107.compute-1.amazonaws.com
 
+# to create instance
+# check the security group
+aws ec2 describe-security-groups
+# if not created, create one
+aws ec2 create-security-group --group-name SallySecurityGroup --description "Sally Security Group" --vpc-id <grab from aws ec2 describe-vpcs>
+
